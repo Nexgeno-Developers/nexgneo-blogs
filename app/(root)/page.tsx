60a4268715310fb1cdfcd2e4973c8d4e7f171fd4
Mohammad suhail <skhan282290@gmail.com>
2024-02-03 16:12:07 +0530

    search comment

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -21,9 +21,9 @@ const HomePage = async ({ searchParams }: HomePageProps) => {
   return (
     <>
       <section>
-        <div className="px-6 pt-6 md:hidden md:mb-0 block">
+        {/* <div className="px-6 pt-6 md:hidden md:mb-0 block">
           <SearchInput />
-        </div>
+        </div> */}
         <div className="container">
           <div className="py-6 space-y-4">
             <CategoriesList items={categories} />
