33d3b262904705c488bf42efb9262d1e5cec6d59
Mohammad suhail <skhan282290@gmail.com>
2024-02-27 16:57:20 +0530

    brevo add

--- lib/mail.ts
+++ lib/mail.ts
@@ -1,14 +1,22 @@
-import { Resend } from "resend";
-
-const resend = new Resend(process.env.RESEND_API_KEY);
-
 const domain = process.env.NEXT_PUBLIC_BASE_URL;
+
+import nodemailer from "nodemailer";
+
+const transporter = nodemailer.createTransport({
+  host: "smtp-relay.brevo.com",
+  port: 587,
+  secure: false,
+  auth: {
+    user: process.env.BREVO_AUTH_USER,
+    pass: process.env.BREVO_AUTH_PASSWORD,
+  },
+});
 
 export const sendPasswordResetEmail = async (email: string, token: string) => {
   const resetLink = `${domain}/auth/new-password?token=${token}`;
 
-  await resend.emails.send({
-    from: "mail@nexgeno.in",
+  await transporter.sendMail({
+    from: "webdeveloper@nexgeno.in",
     to: email,
     subject: "Reset your password",
     html: `<p>Click <a href="${resetLink}">here</a> to reset password.</p>`,
