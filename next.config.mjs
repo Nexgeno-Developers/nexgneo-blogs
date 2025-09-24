1442afac661936e12aa6492562d618b675649a8f
Nexgeno-Developers <webdeveloper@nexgeno.in>
2025-09-24 11:22:20 +0530

    added redirection reules

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,36 @@ const nextConfig = {
   images: {
     domains: ["res.cloudinary.com"],
   },
+
+  async redirects() {
+    return [
+      // Blog root redirect
+      {
+        source: "/",
+        has: [
+          {
+            type: "host",
+            value: "blog.nexgeno.in",
+          },
+        ],
+        destination: "https://nexgeno.in/blog",
+        permanent: true,
+      },
+
+      // Redirect posts to /blog
+      {
+        source: "/posts/:slug*",
+        has: [
+          {
+            type: "host",
+            value: "blog.nexgeno.in",
+          },
+        ],
+        destination: "https://nexgeno.in/blog/:slug*",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
